430ec92cb09ed022b2ba2aa72b824168527ea72a
d91 <dominik.tomcik23@gmail.com>
2025-10-06 22:24:58 +0200

    feat(login): add method to exchange access token for a refresh token

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -1,4 +1,5 @@
 import { env } from '@/env'
+import { getRefreshToken } from '@/lib/session'
 import AuthError from '@/server/auth/error'
 import { LoginSchema, loginSchema } from '@/server/auth/schema'
 import { UserRes } from '@/server/auth/types'
@@ -31,4 +32,27 @@ async function login(payload: LoginSchema) {
   }
 }
 
-export const auth = { login }
+async function getAccessToken() {
+  const refreshToken = await getRefreshToken()
+
+  const userRes = await fetch(`${env.EVENTIO_API_URL}/auth/refresh`, {
+    method: 'POST',
+    headers: {
+      apikey: env.EVENTIO_API_KEY,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ refreshToken }),
+  })
+
+  const userData: UserRes = await userRes.json()
+
+  if ('message' in userData) {
+    throw new AuthError(userData.message)
+  }
+
+  const accessToken = userRes.headers.get('access-token')
+
+  return { accessToken }
+}
+
+export const auth = { login, getAccessToken }
