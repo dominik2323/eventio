45eba4d6fdd9d0a014e1d3f4c113a878dab6f64a
d91 <dominik.tomcik23@gmail.com>
2025-10-09 21:02:09 +0200

    feat: add 500 weight for hind typeface

--- src/consts/fonts.ts
+++ src/consts/fonts.ts
@@ -1,9 +1,11 @@
 import { Hind, Playfair } from 'next/font/google'
 
 export const hind = Hind({
-  weight: '400',
+  weight: ['400', '500'],
+  subsets: ['latin'],
 })
 
 export const playfair = Playfair({
   weight: '400',
+  subsets: ['latin'],
 })
