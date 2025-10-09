dcd4db6fdfef21c489be85db8d1c53af0d4fefce
d91 <dominik.tomcik23@gmail.com>
2025-10-09 21:02:29 +0200

    feat(Button): add button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,18 +1,30 @@
+import { Spinner } from '@/components/Spinner'
+import clsx from 'clsx'
 import type { ButtonHTMLAttributes } from 'react'
+import styles from './Button.module.scss'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'danger' | 'disabled'
+  size?: 'sm' | 'lg'
   loading?: boolean
 }
 
 export function Button({
   children,
+  variant = 'primary',
+  size = 'sm',
   loading = false,
   disabled,
+  className,
   ...props
 }: ButtonProps) {
   return (
-    <button {...props} disabled={disabled || loading}>
-      {loading ? '...' : children}
+    <button
+      {...props}
+      disabled={disabled || loading}
+      className={clsx(styles.button, styles[variant], styles[size], className)}
+    >
+      {loading ? <Spinner className={styles.spinner} /> : children}
     </button>
   )
 }
