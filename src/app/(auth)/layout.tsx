66132f43d8669fd2f912f02c00c44ef19ee8a45a
d91 <dominik.tomcik23@gmail.com>
2025-10-10 12:54:47 +0200

    refactor: create an group for auth routes

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+import HeroLayout from '@/components/Layouts/HeroLayout'
+import React from 'react'
+
+interface PageProps {
+  children: React.ReactNode
+}
+
+async function RootLayout({ children }: PageProps) {
+  return <HeroLayout>{children}</HeroLayout>
+}
+
+export default RootLayout
