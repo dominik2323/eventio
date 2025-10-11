37f730aace15020b372f6b8db5ea03d5fa00b313
d91 <dominik.tomcik23@gmail.com>
2025-10-11 16:14:32 +0200

    feat: add metadata

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,4 +1,10 @@
 import Login from '@/modules/login'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Login',
+  description: 'Login to your Eventio account.',
+}
 
 function LoginPage() {
   return <Login />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
 import { Metadata } from 'next'
 
-export const metadata: Metadata = {}
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Eventio',
+    default: 'Eventio',
+  },
+  description: 'Eventio is an application for managing events.',
+  openGraph: {
+    title: 'Eventio',
+    description: 'Eventio is an application for managing events.',
+    images: '/og-image.png',
+  },
+}
 
 function Page() {
   return <main>Hello World</main>
